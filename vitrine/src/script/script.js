7e04f17225278f9475f6491d4daae1fd1f4b9c91
Kayo Macedo <57056101+kayomacedo@users.noreply.github.com>
2024-05-18 17:18:08 -0300

    pronto
    primeiro desafio pronto

--- vitrine/src/script/script.js
+++ vitrine/src/script/script.js
@@ -1,117 +1,176 @@
-const apiUrl = 'https://desafio.xlow.com.br/search';
-
-async function main() {
+// Função que vai adicionando novos produtos
+async function novoProduto(listaProdutos, quantidadeProdutos,apiUrlBase) {
+    var produtoSelecionado = 0;
+    var container = document.getElementById("container");
     try {
-        const result = await fetch(apiUrl);
+        //Verificação
+        const result = await fetch(apiUrlBase+'/'+listaProdutos.productId);
+        
         if (!result.ok) {
             throw new Error('Erro ao buscar dados da api');
         }
+        //Conversão para Json
         const data = await result.json();
-        data.forEach(item => {
-            add_product(item,data.length)
+        
+        // Iteração para a criação dos produtos
+        data.forEach(async (produto, index) => {
+            
+            // Quantidade
+            var quantidade = document.getElementById("quantidade");
+            quantidade.textContent=quantidadeProdutos+" Produtos"
+            //Container principal
+            var box = document.createElement("div");
+            box.classList.add("box");
+            var titulo = document.createElement("p");
+            //Titulo
+            titulo.classList.add("titulo");
+            titulo.textContent = produto.items[0].nameComplete;
+            // Box imagem principal
+            var boxImagem = document.createElement("div"); 
+            boxImagem.classList.add("boxImagem");
+            var imagem = document.createElement("img");
+            imagem.classList.add("imgPrincipal");
+            imagem.src = produto.items[0].images[produtoSelecionado].imageUrl;
+            boxImagem.appendChild(imagem);
+
+
+            //Box para preços e miniaturas
+            boxContent = document.createElement("div"); 
+            boxContent.classList.add("boxContent");
+            //miniaturas
+            var miniaturas = document.createElement("div");
+            miniaturas.classList.add("miniaturas");
+            //preços
+            var precos = document.createElement("div");
+            precos.classList.add("precos");
+            var preco = document.createElement("h3");
+            var precoComDesconto = document.createElement("h3");
+            precos.appendChild(preco);
+
+            // Evento ao clique na miniatura
+            produto.items[0].images.forEach((detalhesProduto, index) => {
+                var imagemMiniatura = document.createElement("img");
+                imagemMiniatura.classList.add("imgMiniatura");
+                imagemMiniatura.src = detalhesProduto.imageUrl;
+                imagemMiniatura.addEventListener('click', function() {
+                    // atualizando do clique
+                    produtoSelecionado = index;
+                    imagem.src = detalhesProduto.imageUrl;
+                    titulo.textContent = produto.items[0].nameComplete;
+                });
+                miniaturas.appendChild(imagemMiniatura);
+            });
+            // Preco 
+            var precoInicial = produto.items[0].sellers[0].commertialOffer.ListPrice;
+            var precoFormatado = precoInicial.toFixed(2).replace('.', ',');
+            preco.textContent = "R$ " + precoFormatado;
+            // preco desconto
+            if (produto.items[0].sellers[0].commertialOffer.ListPrice != produto.items[0].sellers[0].commertialOffer.PriceWithoutDiscount){
+                var precoDescontInicial = produto.items[0].sellers[0].commertialOffer.PriceWithoutDiscount;
+                var precoFormatado = precoDescontInicial.toFixed(2).replace('.', ',');
+                precoComDesconto.textContent = "R$ " + precoDescontInicial;
+                preco.classList.add("obsoleto");
+                precos.appendChild(precoComDesconto);    
+            }
+
+            precos.appendChild(precoComDesconto);
+
+            //Botão comprar
+
+            var botao = document.createElement("button");
+            botao.classList.add("submit");
+            botao.textContent = "COMPRAR";
+            
+            // adicionando as na box content
+            boxContent.appendChild(titulo);
+            boxContent.appendChild(miniaturas);
+            boxContent.appendChild(precos);
+
+            // adicionando na box principal
+            box.appendChild(boxImagem);
+            box.appendChild(boxContent);
+            box.appendChild(botao);
+            container.appendChild(box);
         });
     } catch (error) {
         console.error('Error:', error);
     }
-
-
-    
 }
 
-
-main()
-
-async function add_product(item, quantidade){
+async function main() {
 
 
-    var nova_quantidade = document.querySelector('.quantidade');
-    nova_quantidade.textContent = quantidade + " produtos";
-
-    // Criar um novo elemento div para o novo box-produto
-    var novoBoxProduto = document.createElement("div");
-    novoBoxProduto.classList.add("box-produto");
-
-    // Criar os elementos internos do novo box-produto
-    var novaImagem = document.createElement("img");
-    novaImagem.classList.add("image");
-    novaImagem.src = item.image;
-
-    var novoTitulo = document.createElement("div");
-    novoTitulo.classList.add("titulo");
-    novoTitulo.textContent = item.productName;
-
-    var miniaturas = document.createElement("div");
-    miniaturas.classList.add('miniaturas')
-
-    const productDetailsResponse = await fetch(`https://desafio.xlow.com.br/search/${item.productId}`);
-    const productDetails = await productDetailsResponse.json();
-
-    console.log(productDetails.items[0])
-
-    if (Array.isArray(productDetails.image)) {
-        item.image.forEach(function(imageUrl) {
-            var miniaturaImagem = document.createElement("img");
-            miniaturaImagem.classList.add("miniatura");
-            miniaturaImagem.src = imageUrl;
-            miniaturas.appendChild(miniaturaImagem);
-        });
-    }else{
-        var miniaturaImagem = document.createElement("img");
-        miniaturaImagem.classList.add("miniatura");
-        miniaturaImagem.src = item.image;
-        miniaturas.appendChild(miniaturaImagem) 
-    }
-
-
-
-
-    if(item.bestPrice != item.listPrice){
-        var precoAntigo = document.createElement("div");
-        precoAntigo.classList.add("preco-antigo");
-        var precoCentavos = item.listPrice;     
-        var precoReais = precoCentavos / 100;
-        var precoFormatado = precoReais.toFixed(2).replace('.', ',');
-        precoAntigo.textContent = "R$ " + precoFormatado;
-    }
+    const apiUrlBase = 'https://desafio.xlow.com.br/search';
     
+    // Variaveis para controle de paginas
+    var productPage= 5
+    var inicialCurrentProduct =0
+    var pages =[]
 
-    var novoPreco = document.createElement("div");
-    novoPreco.classList.add("preco");
-    var precoCentavos = item.bestPrice;     
-    var precoReais = precoCentavos / 100;
-    var precoFormatado = precoReais.toFixed(2).replace('.', ',');
-    novoPreco.textContent = "R$ " + precoFormatado;
-
-    var button = document.createElement("button");
-    button.classList.add("submit");
-    button.textContent = "COMPRAR";
-
-
+    var endCurrentProduct = inicialCurrentProduct + productPage
     try {
-         // Adicionar os elementos internos ao novo box-produto
-            novoBoxProduto.appendChild(novaImagem);
-            novoBoxProduto.appendChild(novoTitulo);
-            novoBoxProduto.appendChild(miniaturas)
-            novoBoxProduto.appendChild(precoAntigo);
-            novoBoxProduto.appendChild(novoPreco);
-            novoBoxProduto.appendChild(button);
+        const result = await fetch(apiUrlBase);
+        if (!result.ok) {
+            throw new Error('Erro ao buscar dados da api');
+        }
+        const data = await result.json();
+        const quantidadeProdutos = data.length;
+        
+        
 
+         // cada pagina ficada com 5 produtos
+        const protudosAtuais = data.slice(inicialCurrentProduct, endCurrentProduct);
+        
+        // Array para ficar com as paginas 
+        var totalPaginas = Math.ceil(quantidadeProdutos / productPage);
+        var paginacao = document.getElementById("pagination");
+
+        for (let i = 1; i <= totalPaginas; i++) {
+            pages.push(i)
+        }
+
+        
+        pages.forEach((pagina, index) => {
+            var item = document.createElement("h2")
+            item.textContent=pagina
+
+            // evento ao clicar na pagina
+            item.addEventListener('click', function() {
+                var boxes = document.querySelectorAll('.box');
+
+                // Limpando 
+                boxes.forEach(function(box) {
+                    box.remove();
+                });
+
+                
+
+                
+                endCurrentProduct = productPage * (index+1)
+                inicialCurrentProduct = endCurrentProduct - productPage
+                const protudosAtuais = data.slice(inicialCurrentProduct, endCurrentProduct);
+                //atualizando
+                protudosAtuais.forEach(produto => {
+                    novoProduto(produto,quantidadeProdutos,apiUrlBase);
+                });
+                
+            });
+
+            paginacao.appendChild(item)
+        });
+        
+        
+      
+        
+
+        protudosAtuais.forEach(produto => {
+            novoProduto(produto,quantidadeProdutos,apiUrlBase);
+        });
+
+        
     } catch (error) {
-        // Adicionar os elementos internos ao novo box-produto
-            novoBoxProduto.appendChild(novaImagem);
-            novoBoxProduto.appendChild(novoTitulo);
-            novoBoxProduto.appendChild(miniaturas)
-            novoBoxProduto.appendChild(novoPreco);
-            novoBoxProduto.appendChild(button);
+        console.error('Error:', error);
     }
-
-
-    
-
-    // Adicionar o novo box-produto à div .minha-vitrine
-    var minhaVitrine = document.querySelector(".minha-vitrine");
-    minhaVitrine.appendChild(novoBoxProduto);
 }
 
-
-
+main();
